c8a1840a9dc4c1ca90b9c728e63f9bffda88f8e0
marossvk <marosjanota@gmail.com>
2015-12-16 10:17:08 +0100

    vzhlad +favicon

--- calendar/main/Calendar.java
+++ calendar/main/Calendar.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Calendar extends Application {
@@ -17,6 +18,7 @@ public class Calendar extends Application {
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
+        stage.getIcons().add(new Image("images/btns/favico.png"));
         stage.show();
     }
 
